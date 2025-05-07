4e0c10bf5aaba3d7a63a01f84a2d628e9ffcb5f5
Christian Grail <cgrail@users.noreply.github.com>
2025-05-07 18:04:21 +0200

    Add LmStudio Depdency

--- ui/App.tsx
+++ ui/App.tsx
@@ -1,7 +1,18 @@
 import { Button, Icon, List, ListItemStandard, ShellBar } from '@ui5/webcomponents-react';
 import React from 'react';
+import { LLM, LMStudioClient } from "@lmstudio/sdk";
 
 function App() {
+
+  async function initLm() {
+
+    const client = new LMStudioClient({ baseUrl: "ws://127.0.0.1:1234" });
+    const llmModel = await client.llm.model("qwen3-4b");
+    const result = await llmModel.respond("What is the meaning of life?");
+    alert(result.content);
+  }
+  initLm();
+
   return (
     <>
       <ShellBar primaryTitle="MCP Chat using UI5 Web Components" logo={<Icon name="ai" />}></ShellBar>
